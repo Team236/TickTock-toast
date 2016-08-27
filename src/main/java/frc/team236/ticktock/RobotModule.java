a9dac435ba3b995ec1cd86779db50ac0644b0cc2
Sam Fuchs <samcfuchs@gmail.com>
2016-08-27 11:26:21 -0400

    Initial commit

---
+++ src/main/java/frc/team236/ticktock/RobotModule.java
@@ -0,0 +1,25 @@
+package frc.team236.ticktock;
+
+import jaci.openrio.toast.lib.log.Logger;
+import jaci.openrio.toast.lib.module.IterativeModule;
+
+public class RobotModule extends IterativeModule {
+
+    public static Logger logger;
+
+    @Override
+    public String getModuleName() {
+        return "TickTock";
+    }
+
+    @Override
+    public String getModuleVersion() {
+        return "0.0.1";
+    }
+
+    @Override
+    public void robotInit() {
+        logger = new Logger("TickTock", Logger.ATTR_DEFAULT);
+        //TODO: Module Init
+    }
+}
